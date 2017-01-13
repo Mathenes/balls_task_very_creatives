d26a37e8907f40ee60db7c1acd5d932f148ed78c
matheusviana <matheusviana@id.uff.br>
2017-01-13 13:58:51 -0200

    XMLHttpRequest implemented in BallsManager class and AJAX requisition working

--- app/assets/javascripts/components/balls_manager.js.jsx
+++ app/assets/javascripts/components/balls_manager.js.jsx
@@ -8,11 +8,25 @@ class BallsManager extends React.Component {
     return this.scores[BallsManager.getKeyByValue(this.colors, color)];
   }
 
+  static accessServer(ballsCounter, success, token) {
+    const params = JSON.stringify({ balls_core: { sum: ballsCounter } });
+    const COMPLETE_URL = '/balls_core/send_sum';
+    this.xhr.open('POST', COMPLETE_URL, false);
+    this.xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+    this.xhr.setRequestHeader('X-CSRF-TOKEN', token);
+    this.xhr.send(params);
+    if (this.xhr.status === 200) {
+      const DATA = this.xhr.responseText;
+      success(DATA);
+    }
+  }
+
   constructor() {
     super();
     this.handleSubmit = this.handleSubmit.bind(this);
     BallsManager.getKeyByValue = BallsManager.getKeyByValue.bind(this);
     BallsManager.getScoreByColor = BallsManager.getScoreByColor.bind(this);
+    BallsManager.accessServer = BallsManager.accessServer.bind(this);
     this.colors = {
       pink: 'pink',
       green: 'lightgreen',
@@ -25,18 +39,29 @@ class BallsManager extends React.Component {
       blue: 5,
       purple: 15,
     };
+    this.xhr = new XMLHttpRequest();
     this.state = { ballsCount: 0, score: 0, color: '' };
+  }
+
+  componentDidMount() {
+    this.token = document.getElementsByName('csrf-token')[0].content;
   }
 
   handleSubmit(event) {
     event.preventDefault();
 
-    // simulating JSON response => just a test
-    const color = 'pink';
     const ballsCount = this.state.ballsCount + 1;
-    const score = this.state.score + BallsManager.getScoreByColor(color);
-    //-----------------------------------------
+    let score = this.state.score;
+    let color = '';
 
+    BallsManager.accessServer(
+      ballsCount,
+      (data) => {
+        color = JSON.parse(data).color;
+        score += BallsManager.getScoreByColor(color);
+      },
+      this.token,
+    );
     this.setState({ ballsCount, score, color });
   }
 
--- app/assets/javascripts/components/canvas.js.jsx
+++ app/assets/javascripts/components/canvas.js.jsx
@@ -1,13 +1,11 @@
 class Canvas extends React.Component {
 
   render() {
-    // simulating props from balls_manager and simulating draw of a ball => just a test!
     const color = this.props.color;
     if (color) {
       const c = new Circle(color, this.canvas);
       c.draw();
     }
-    //-----------------------------------------
 
     return (
       <canvas id="balls_canvas" height="100" ref={input => this.canvas = input}>
